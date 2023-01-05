bae793d0554f0f49c149890c7d50efe31ebcf439
Redskull-127 <meerraja17@gmail.com>
2023-01-05 20:32:24 +0530

    API routed

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import jsonData from './data.json'
+import jsonData from '../../public/data.json'
 
 export default function handler(req, res) {
   res.status(200).json({jsonData})
