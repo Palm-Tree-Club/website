187e9be725dc6aea98fd4fedea48c051d330b7dc
Redskull-127 <meerraja17@gmail.com>
2023-01-05 20:24:55 +0530

    Progress 80%

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 export default function Contact() {
     return (
-        <div className="w-screen h-96"></div>
+        <div id='contact' className="w-screen h-96 flex justify-center items-center">
+            <h1 className="text-6xl font-bold uppercase text-black">Contact us</h1>
+        </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 import * as Scroll from "react-scroll";
 import { Link, animateScroll as scroll } from "react-scroll";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+    const router = useRouter();
     return (
         <nav className={`fixed flex p-10 justify-between items-center w-full h-20 bg-zinc-900 font-sans font-semibold text-lg`}>
-            <a className="text-cyan-50 text-2xl">Logo | <img onClick={()=>{window.open('https://silveroakuni.ac.in', '_blank')}} src="/Assets/Images/sou.png" className="inline-block cursor-pointer" height={100} width={100} alt="silver oak university"/></a>
+            <a className="text-cyan-50 text-2xl"><img className="inline-block cursor-pointer" onClick={() => {
+                router.reload();
+            }} src="/Assets/Images/palm.png" height={50} width={50} alt="Palm Tree Club" /> | <img onClick={() => { window.open('https://silveroakuni.ac.in', '_blank') }} src="/Assets/Images/sou.png" className="inline-block cursor-pointer" height={100} width={100} alt="silver oak university" /></a>
             <ul className="flex justify-center text-cyan-50">
                 <Link activeClass="active" to="hero" smooth={true} duration={300} className="mr-10 cursor-pointer hover:text-sky-700 transition duration-300">Home</Link>
                 <Link activeClass="active" to="domain" smooth={true} duration={300} className="mr-10 cursor-pointer hover:text-sky-700 transition duration-300">Domains</Link>
                 <Link activeClass="active" to="achivements" smooth={true} duration={300} className="mr-10 cursor-pointer hover:text-sky-700 transition duration-300">Achievements</Link>
                 <Link activeClass="active" to="events" smooth={true} duration={300} className="mr-10 cursor-pointer hover:text-sky-700 transition duration-300">Events</Link>
-                <Link activeClass="active" to="" smooth={true} duration={300} className="cursor-pointer hover:text-sky-700 transition duration-300">Contact</Link>
+                <Link activeClass="active" to="contact" smooth={true} duration={300} className="cursor-pointer hover:text-sky-700 transition duration-300">Contact</Link>
             </ul>
         </nav>
     )
