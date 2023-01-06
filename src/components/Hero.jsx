7b7a982927274a7c1c11f61f7473a06008a93b07
Redskull-127 <meerraja17@gmail.com>
2023-01-06 19:26:35 +0530

    ImgBot

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
                 <a className="hidden mb-10 max-sm:inline-block text-cyan-50 text-2xl"><img className="inline-block cursor-pointer" src="/Assets/Images/palm.png" height={100} width={100} alt="Palm Tree Club" /> | <img onClick={() => { window.open('https://silveroakuni.ac.in', '_blank') }} src="/Assets/Images/sou.png" className="inline-block cursor-pointer" height={150} width={150} alt="silver oak university" /></a>
                 <h1 className="text-white text-6xl max-sm:text-3xl max-sm:text-center font-extrabold uppercase"><a href="https://silveroakuni.ac.in" target={'_blank'} className="hover:text-sky-700 transition duration-300 cursor-pointer" rel="noreferrer">SOCCA&apos;s </a>Largest Community</h1>
                 <h1 className="mt-10 text-7xl max-sm:text-3xl uppercase font-bold text-ambers">Palm Tree Club</h1>
-                <p className="text-white mt-10 text-3xl max-sm:text-3xl font-semibold text-grey">Learn | Build | Grow</p>
+                <p className="text-white mt-16 text-3xl max-sm:text-3xl font-semibold text-grey">Learn | Build | Grow</p>
             </div>
         </div>
     )
