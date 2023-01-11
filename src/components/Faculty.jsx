1c658f5d90be4742f635c1ee9522e5f2d7e5f7ad
Redskull-127 <meerraja17@gmail.com>
2023-01-11 15:03:29 +0530

    Upgrades

--- src/components/Faculty.jsx
+++ src/components/Faculty.jsx
@@ -4,8 +4,9 @@ export default function Faculty() {
     <div id="faculty" className="select-none h-screen w-screen flex flex-col justify-center items-center bg-purple-800">
       <h1 data-aos='fade-up' className="md-sm:text-3xl text-5xl text-white">Meet our Founders, Organizers & Leaders</h1>
       <div className='w-screen m-14 flex justify-evenly items-center'>
-        <img data-aos='fade-right' className="cursor-pointer" src='/Assets/Images/cards/principal.png' width={230} height={230} alt='principal'/>
-        <img data-aos='fade-up' className="cursor-pointer" src='/Assets/Images/cards/HOD.png' width={230} height={230} alt='HOD'/>
+        <img data-aos='fade-right' className="cursor-pointer" src='/Assets/Images/cards/dean.png' width={230} height={230} alt='Dean'/>
+        <img data-aos='fade-up' className="cursor-pointer" src='/Assets/Images/cards/principal.png' width={230} height={230} alt='principal'/>
+        <img data-aos='fade-down' className="cursor-pointer" src='/Assets/Images/cards/HOD.png' width={230} height={230} alt='HOD'/>
         <img data-aos='fade-left' className="cursor-pointer" src='/Assets/Images/cards/meghnamam.png' width={230} height={230} alt='meghna mam'/>
       </div>
     </div>
